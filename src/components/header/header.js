2628afd0bc1167c3db312dfd58d4784f91678b11
Raymond <raymond.lo@yahoo.com>
2021-08-02 23:13:58 -0400

    exported header render

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,14 @@
 import "./header.css";
 import Logo from "./top-logo.svg";
 
-const body = document.querySelector("body");
+export const renderHeader = (container) => {
+  const logoIcon = new Image();
+  logoIcon.src = Logo;
+  logoIcon.classList.add("todo-logo");
 
-const logoIcon = new Image();
-logoIcon.src = Logo;
-logoIcon.classList.add("todo-logo");
+  const header = document.createElement("header");
+  header.classList.add("todo-header");
+  header.appendChild(logoIcon);
 
-const header = document.createElement("header");
-header.classList.add("todo-header");
-header.appendChild(logoIcon);
-
-body.appendChild(header);
+  container.appendChild(header);
+};

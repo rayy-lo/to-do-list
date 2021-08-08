5a20866133935afaffad98540fd1a0b14fe7b642
Raymond <raymond.lo@yahoo.com>
2021-08-07 21:09:22 -0400

    cleaned up file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import "normalize.css";
 import "./index.css";
 import PubSub from "pubsub-js";
+import { loadTasks, loadProjects } from "./utils/utils";
 
-import { renderHeader } from "./components/header/header";
-import { renderSidebar } from "./components/sidebar/sidebar";
-
-const body = document.querySelector("body");
-const main = document.createElement("main");
-
-renderHeader(body);
-
-body.appendChild(main);
-renderSidebar(main);
-
-// setup test data on page load
+loadTasks();
+loadProjects();

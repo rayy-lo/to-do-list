8e340899f297a977ebaff7491444e04e9c075669
Raymond <raymond.lo@yahoo.com>
2021-08-09 00:34:48 -0400

    imported project display module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "normalize.css";
 import "./index.css";
+import "./components/addProject/addProject";
+import "./components/projectDisplay/projectDisplay";
 import PubSub from "pubsub-js";
 import { loadTasks, loadProjects } from "./utils/utils";
 

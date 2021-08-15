d4f7181a03567683841b895a80746858bb7886d5
Raymond <raymond.lo@yahoo.com>
2021-08-15 02:11:08 -0400

    Added projectrender section

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,3 +1,38 @@
 import "./projects.css";
+import RightArrowIcon from "./right-arrow.png";
+import { getProjects } from "../../utils/utils";
+import { NEW_PROJECT } from "../../utils/events";
 
-export const renderProjects = (projects) => {};
+const container = document.querySelector(".projects-section");
+
+export const renderProjectSection = () => {
+  const data = getProjects();
+  if (!data) return;
+
+  const markup = `
+    ${data.map(
+      (project) =>
+        `
+        <div class="sidebar-project">
+          <button aria-controls="${
+            project.projectName
+          }-tasks" type="button" class="sidebar-projectName">
+              <img class="right-carat" src=${RightArrowIcon}>
+              <p>
+                  ${project.projectName}
+                  <span>${project.tasks.length + 1}</span>
+              </p>
+          </button>
+          <div id="${project.projectName}-tasks" class="sidebar-tasks">
+            
+          </div>
+        </div>
+        `
+    )}
+`;
+
+  console.log(data);
+  container.innerHTML = markup;
+};
+
+PubSub.subscribe(NEW_PROJECT, renderProjectSection);

03b7af0a625176b9d1331f57f7dc6d9b6f14024c
Raymond <raymond.lo@yahoo.com>
2021-08-20 01:28:28 -0400

    Finished form handle

--- src/components/projectDisplay/projectDisplay.js
+++ src/components/projectDisplay/projectDisplay.js
@@ -1,17 +1,42 @@
 import "./projectDisplay.css";
+import { Task } from "../../models/task";
+import { storeTask } from "../../utils/utils";
+import { NEW_TASK } from "../../utils/events";
 
 const attachEventListeners = () => {
   const closeProjectFormBtn = document.querySelector(
     ".task-form > .close-form-btn"
   );
   const addTaskBtn = document.querySelector(".add-task-btn");
+  const taskForm = document.querySelector(".task-form");
+
+  taskForm.addEventListener("submit", handleTaskFormSubmit);
   addTaskBtn.addEventListener("click", toggleFormDisplay);
   closeProjectFormBtn.addEventListener("click", toggleFormDisplay);
 };
 
 const handleTaskFormSubmit = (e) => {
   e.preventDefault();
-  console.log("create task");
+  const projectID = document
+    .querySelector(".add-task-btn")
+    .getAttribute("data-project-id");
+
+  const form = e.target;
+  const formData = new FormData(form);
+
+  const taskName = formData.get("task-name");
+  const dueDate = formData.get("dueDate");
+  const priority = formData.get("priority");
+  const description = formData.get("task-description");
+
+  const newTask = Task(taskName, description, dueDate, priority);
+  storeTask(projectID, newTask);
+  Store.updateState();
+
+  form.reset();
+  form.parentElement.classList.toggle("open");
+
+  PubSub.publish(NEW_TASK, newTask);
 };
 
 const toggleFormDisplay = (e) => {
@@ -42,7 +67,9 @@ export const renderProjectDisplay = (e) => {
               )
         }
     </div>
-    <button aria-controls="task-form-overlay" type="button" class="add-task-btn">
+    <button data-project-id=${
+      project.id
+    } aria-controls="task-form-overlay" type="button" class="add-task-btn">
         Add Task
     </button>
   `;
